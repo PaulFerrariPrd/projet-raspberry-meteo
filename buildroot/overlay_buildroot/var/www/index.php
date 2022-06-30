1fb8b84650617c88ed14b37de4eb3dc21df44fbd
paul <pauljpferrari@gmail.com>
2022-06-30 12:15:58 +0200

    add ssh keys + script to get temp & pressure

--- buildroot/overlay_buildroot/var/www/index.php
+++ buildroot/overlay_buildroot/var/www/index.php
@@ -1,1 +1,25 @@
-<?php phpinfo(); ?>
+<form method="post" action="">
+   <div>
+       <label for="pays">Quel intervalle entre mesure température?</label><br />
+       <select name="period" id="period">
+	   <option value="3">3sec</option>
+	   <option value="6">6sec</option>
+           <option value="30">30sec</option>
+           <option value="60">1min</option>
+           <option value="120">2min</option>
+           <option value="240">4min</option>
+           <option value="3600">1h</option>
+           <option value="7200">2hs</option>
+       </select>
+   </div>
+   <div>
+        <button type="submit">Select</button>
+   </div>
+</form>
+
+<?php
+$fp = fopen('/data/config_c', 'w');
+fwrite($fp, "PERIOD=" . $_POST['period'] . "\n");
+fwrite($fp, "# intervalle entre chaque mesure en secondes\n");
+fclose($fp);
+?>
